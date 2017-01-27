9a1d7eb8e69ac930024e8eee088a00c0ede167ee
Asit Kumar Parida <asitparida@live.in>
2017-01-27 16:34:39 +0530

    Adding Cordova Project

---
+++ MultihostAdapter/MultihostAdapter.Cordova/www/dist/js/directives/ng-long-hover.directive.js
@@ -0,0 +1,22 @@
+﻿angular
+.module('MusicUI')
+.directive('ngLongHover', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function ($scope, $elm, $attrs) {
+            $elm.bind('mouseenter', function (evt) {
+                $scope.longHover = true;
+                $timeout(function () {
+                    if ($scope.longHover) {
+                        $scope.$apply(function () {
+                            $scope.$eval($attrs.ngLongHover)
+                        });
+                    }
+                }, 1500);
+            });
+            $elm.bind('mouseleave', function (evt) {
+                $scope.longHover = false;
+            });
+        }
+    }
+}]);
